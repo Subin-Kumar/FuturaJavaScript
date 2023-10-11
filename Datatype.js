8bb2d46e48efc57d96c18ed53217f1ebceb84279
Subin-Kumar <137891964+Subin-Kumar@users.noreply.github.com>
2023-10-11 21:38:08 +0530

    11/10

---
+++ Datatype.js
@@ -0,0 +1,8 @@
+var num=1
+var name="subi"
+var b=true
+var c
+document.write(typeof num+"<br>")
+document.write(typeof name+"<br>")
+document.write(typeof b+"<br>")
+document.write(typeof c+"<br>")
---
+++ variables.js
@@ -0,0 +1,12 @@
+var num=10
+var num=20 //redeclare
+num=30     //reassign
+
+let st="subin"
+// let st="smudge"  //No redeclare
+st="smudge"         //reassign
+
+const pi=3.14
+
+a=10
+document.write("num "+num +"<br>"+"string "+st+"<br>"+"pi "+pi +"<br>"+"a "+a )
